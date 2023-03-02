38a1148ca340cd0c0d589d9910907fe384bf66fd
fbwfresh <joshua.dixon@mykenzie.snhu.edu>
2023-03-01 22:55:05 -0800

    videoGameRecord file added

--- Application/src/main/java/com/kenzie/appserver/repositories/model/VideoGameRecord.java
+++ Application/src/main/java/com/kenzie/appserver/repositories/model/VideoGameRecord.java
@@ -3,10 +3,8 @@ package com.kenzie.appserver.repositories.model;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBAttribute;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBHashKey;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBTable;
-import com.google.common.base.Strings;
 import com.kenzie.appserver.service.model.Consoles;
 
-import java.io.Console;
 import java.util.ArrayList;
 import java.util.List;
 
