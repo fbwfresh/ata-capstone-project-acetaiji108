b327e5ef7022468d44cffdee49b28ad66c07293e
Ace Vallega <acetaiji108@gmail.com>
2023-03-15 20:29:37 -0700

    Made Game Class in ServiceLambdaModel

---
+++ ServiceLambdaModel/src/main/java/com/kenzie/capstone/service/model/Game.java
@@ -0,0 +1,67 @@
+package com.kenzie.capstone.service.model;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.List;
+import java.util.Set;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class Game {
+    @JsonProperty("name")
+    private String name;
+    @JsonProperty("Consoles")
+    private Set<String> consoles;
+    @JsonProperty("Description")
+    private String description;
+    @JsonProperty("UpwardVote")
+    private int upwardVote;
+    @JsonProperty("DownwardVote")
+    private int downwardVote;
+    @JsonProperty("TotalVote")
+    private int totalVote;
+
+
+    public Set<String> getConsoles(){return consoles;}
+
+    public void setConsoles(Set<String> consoles){this.consoles = consoles;}
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getUpwardVote() {
+        return upwardVote;
+    }
+
+    public void setUpwardVote(int upwardVote) {
+        this.upwardVote = upwardVote;
+    }
+
+    public int getDownwardVote() {
+        return downwardVote;
+    }
+
+    public void setDownwardVote(int downwardVote) {
+        this.downwardVote = downwardVote;
+    }
+
+    public int getTotalVote() {
+        return totalVote;
+    }
+
+    public void setTotalVote(int totalVote) {
+        this.totalVote = totalVote;
+    }
+}
